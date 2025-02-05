4e9737b04f634cdd05b2296a2b88dc83fe993dcd
Arely370 <arelitt.ti21@utsjr.edu.mx>
2025-02-05 12:17:07 -0600

    Primer commit: proyecto Angular inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,97 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejemplo03';
+  title = 'ejemplo3';
+
+  // Propiedades
+  producto = {
+    id: 0,
+    descripcion: '',
+    precio: 0
+  };
+
+  productos = [
+    { id: 1, descripcion: 'Gansito Marinela', precio: 15.50 },
+    { id: 2, descripcion: 'Pepsi', precio: 15.50 },
+    { id: 3, descripcion: 'Coca', precio: 15.50 },
+    { id: 4, descripcion: 'Tacos', precio: 15.50 },
+    { id: 5, descripcion: 'Pinguinitos', precio: 15.50 },
+  ];
+
+  // Función para agregar un producto al arreglo
+  agregarProducto() {
+    if (this.producto.id == 0) {
+      alert('El ID debe ser diferente a cero');
+      return;
+    }
+
+    // Verificamos que el ID no sea repetido
+    for (let i = 0; i < this.productos.length; i++) {
+      if (this.producto.id == this.productos[i].id) {
+        alert('Ya existe un producto con ese ID');
+        return;
+      }
+    }
+
+    // Agregamos el producto al arreglo
+    this.productos.push({
+      id: this.producto.id,
+      descripcion: this.producto.descripcion,
+      precio: this.producto.precio
+    });
+
+    // Reseteamos el objeto producto
+    this.producto.id = 0;
+    this.producto.descripcion = '';
+    this.producto.precio = 0;
+  }
+
+  // Función para seleccionar un producto existente
+  seleccionarProducto(productoSeleccionado: { id: number; descripcion: string; precio: number }) {
+    this.producto.id = productoSeleccionado.id;
+    this.producto.descripcion = productoSeleccionado.descripcion;
+    this.producto.precio = productoSeleccionado.precio;
+  }
+
+  // Función para modificar un producto existente
+  modificarProducto() {
+    let encontrado = false;
+    for (let i = 0; i < this.productos.length; i++) {
+      if (this.producto.id == this.productos[i].id) {
+        this.productos[i].descripcion = this.producto.descripcion;
+        this.productos[i].precio = this.producto.precio;
+        encontrado = true;
+
+        // Reseteamos el objeto producto
+        this.producto.id = 0;
+        this.producto.descripcion = '';
+        this.producto.precio = 0;
+        break; // Salimos del bucle
+      }
+    }
+    if (!encontrado) {
+      alert('No existe un producto con ese ID');
+    }
+  }
+
+  // Función para eliminar un producto del arreglo
+  eliminarProducto(id: number) {
+    for (let i = 0; i < this.productos.length; i++) {
+      if (id == this.productos[i].id) {
+        this.productos.splice(i, 1);
+        alert('Producto eliminado correctamente');
+        return;
+      }
+    }
+    alert('No existe un producto con ese ID');
+  }
 }
